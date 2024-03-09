04dfbe5324e1cf16143445875922c2be1f7957b7
AnshitaKanthed <anshita.j08@gmail.com>
2024-03-09 20:41:27 +0530

    Day 5

---
+++ Day5/Hashing/count_hash.cpp
@@ -0,0 +1,65 @@
+// Given an array, we have found the number of occurrences of each element in the array.
+
+// Solution 1: Use of two loops
+
+// Time Complexity: O(N*N)
+// Space Complexity:  O(N)
+#include <bits/stdc++.h>
+using namespace std;
+
+void countFreq(int arr[], int n)
+{
+    vector<bool> visited(n, false);
+    for (int i = 0; i < n; i++) {
+        // Skip this element if already processed
+        if (visited[i] == true)
+            continue;
+        // Count frequency
+        int count = 1;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                visited[j] = true;
+                count++;
+            }
+        }
+        cout << arr[i] << " " << count << endl;
+    }
+}
+
+int main()
+{
+    int arr[] = {10,5,10,15,10,5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    countFreq(arr, n);
+    return 0;
+}
+
+
+
+// Solution 2: Using Map
+
+// Time Complexity: O(N)
+// Space Complexity: O(n)
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void Frequency(int arr[], int n)
+{
+    unordered_map<int, int> map;
+ 
+    for (int i = 0; i < n; i++)
+        map[arr[i]]++;
+ 
+    // Traverse through map and print frequencies
+    for (auto x : map)
+        cout << x.first << " " << x.second << endl;
+}
+ 
+int main()
+{
+    int arr[] = {10,5,10,15,10,5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    Frequency(arr, n);
+    return 0;
+}
